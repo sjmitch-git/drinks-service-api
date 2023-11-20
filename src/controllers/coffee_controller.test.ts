fb9fcbd9b6349aae1ce33352e861712b985af521
sjmitch-git <sjmitch20@outlook.com>
2023-11-20 14:30:54 +0000

    refactor: coffee controller test

--- src/controllers/coffee_controller.test.ts
+++ src/controllers/coffee_controller.test.ts
@@ -9,3 +9,21 @@ test("GET /coffee should return correct object", async () => {
     name: "Latte",
   });
 });
+
+test("GET /coffee should return correct object", async () => {
+  const res = await request(app).get("/coffee").query({ coffeeName: "Mocha" });
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Mocha",
+  });
+});
+
+test("GET /coffee should return default object", async () => {
+  const res = await request(app).get("/coffee");
+  expect(res.statusCode).toEqual(200);
+  expect(res.body).toEqual({
+    drinkType: "Coffee",
+    name: "Latte",
+  });
+});
