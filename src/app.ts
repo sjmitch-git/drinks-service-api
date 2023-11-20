e62625c9afb8b7f32212a28ecd2c248dc77f2140
sjmitch-git <sjmitch20@outlook.com>
2023-11-20 13:35:20 +0000

    feat: Implement GET request for coffeelover endpoint and test

--- src/app.ts
+++ src/app.ts
@@ -3,3 +3,5 @@ import express from "express";
 export const app = express();
 
 app.get("/", (req, res) => res.send("Welcome to the Drinks API!"));
+
+app.get("/coffeelover", (req, res) => res.send("I like coffee!"));
